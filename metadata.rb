0bb04ffb308178da9b69ed7d2131972cb25a96bd
Tyler Sanders <tylerssanders@gmail.com>
2013-05-20 22:48:10 -0500

    init commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "jrockit"
+maintainer       "YOUR_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "Apache 2.0"
+description      "Installs/Configures jrockit"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0.1.0"
+
